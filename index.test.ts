d9373910e3514a622543e4f1722161b6774557ca
Robert Dailey <dale13@gmail.com>
2022-07-14 14:08:43 -0700

    Implemented API and corresponding test.

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,8 @@
-import {hello} from './index';
+import { hello } from './index';
+import * as config from './default.json';
+
+const PORT = config.server.port;
+const HOST = config.server.host;
 
 describe("Default index test", () => {
     it("should pass", () => {
@@ -10,5 +14,12 @@ describe("Testing index hello", () => {
     it("should be found and return hello", () => {
         const word = hello();
         expect(word).toBe('Hello');
+    })
+})
+
+//TODO mock call vs doing an actual one
+describe("Testing main functions", () => {
+    it ('should return json object with a message and timestamp', async () => {
+        expect(fetch(`http://${HOST}:${PORT}`)).resolves.toEqual({message: "Automate all things!", timestamp: expect.any(Number)});        
     })
 })
--- index.ts
+++ index.ts
@@ -1,3 +1,28 @@
+import express, { Express, Request, Response }  from 'express';
+import cors from 'cors';
+import * as config from './default.json';
+
+const PORT = config.server.port;
+const app: Express = express();
+
+app.disable('x-powered-by')
+app.use(
+    cors(),
+    express.json(),
+    express.urlencoded({extended: true})
+);
+
+
+//TODO: JWT and security
+
+app.get('/', (req: Request, res: Response, next) => {
+    res.json({message: "Automate all things!", timestamp: new Date().getTime()});
+    //next();
+});
+
+export const server = app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
+
+
 export function hello() {
     return 'Hello';
 }
